a5fdf0d5e8e89db4e229b0479c46eddfd1a1aba1
josevillegasjavc-max <josevillegasjavc@gmail.com>
2026-03-15 19:24:33 -0400

    version 0.1.3

--- script/main.js
+++ script/main.js
@@ -1,21 +1,33 @@
 function crearPlantilla(titulo, actividad) {
     return `
     <article class="cardsActivity">
-        <h2 class="titleActivity" >${titulo}</h2>
+        <h3 class="titleActivity" >${titulo}</h3>
         <p class="description" >${actividad}</p>
-    </arcticle>
+    </article>
     `;
 }
 
+let iniciado = false;
+
 function mostrarInfo() {
 event.preventDefault();
+
+if (iniciado == false) {
+    let subtitulo = document.getElementById("contTitleSection");
+    subtitulo.innerHTML = `
+    <h2 id="titleSection"> 
+        <span id="spantitle"> LISTA DE ACTIVIDADES </span> 
+    </h2>
+    `;
+}
 
 let inputTitle = document.getElementById("title");
 let inputDescription = document.getElementById("inputDescription");
 let contenedor = document.getElementById("datos");
 
+
 if (inputTitle.value === "" || inputDescription.value === "") {
-    alert("Por favor, llena todos los campos");
+    alert("Por favor, llena todos losr campos");
     return;
 }
 
@@ -24,5 +36,3 @@ contenedor.insertAdjacentHTML('afterbegin', crearPlantilla(inputTitle.value, inp
 inputTitle.value = "";
 inputDescription.value = "";
 }
-
-
