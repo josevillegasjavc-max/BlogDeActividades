c216503caa9954c5aa87d980050fab8b041fcc35
josevillegasjavc-max <josevillegasjavc@gmail.com>
2026-03-20 00:13:14 -0400

    version 0.1.5

---
+++ script/buttons.js
@@ -0,0 +1,50 @@
+const contenedorDatos = document.getElementById("datos");
+
+const manejarClickEnContenedor = (event) => {
+    const elementoClickado = event.target;
+
+    const tarjeta = elementoClickado.closest(".cardsActivity");
+
+    if (elementoClickado.classList.contains("buttonX")) {   
+        if (tarjeta) {
+            tarjeta.remove();
+        }
+    }
+
+    if (elementoClickado.classList.contains("buttonE")) {
+
+        const title = tarjeta.querySelector(".titleActivity");
+        const descripcion = tarjeta.querySelector(".description");
+        
+        descripcion.classList.toggle("edit");
+        title.classList.toggle("edit");
+
+        if (descripcion.contentEditable !== "true") {
+
+            title.contentEditable = "true";
+            descripcion.contentEditable = "true";
+            descripcion.focus();  
+
+            elementoClickado.src = "./resource/disco.png";
+            elementoClickado.title = "Guardar";
+            const removePoint = title.querySelector(".twoPoints")
+            removePoint.remove();
+
+        } else {
+            /*  
+                descripcion.textContent = descripcion.textContent.trim();
+                title.textContent = title.textContent.trim(); 
+            */
+            
+            title.insertAdjacentHTML('beforeend','<span class="twoPoints">:</span>');
+
+            elementoClickado.src = "./resource/lapiz.png";
+            elementoClickado.title = "Editar";
+
+            descripcion.contentEditable = "false";
+            title.contentEditable = "false";
+        }
+    }
+};
+
+contenedorDatos.addEventListener("click", manejarClickEnContenedor);
---
+++ script/cardsAgree.js
@@ -0,0 +1,39 @@
+function crearPlantilla(titulo, actividad) {
+    return `
+    <article class="cardsActivity">
+        <div class="infoContainer">
+            <h3 class="titleActivity">
+                ${titulo}<span class="twoPoints">:</span>
+            </h3>
+            <p class="description" >${actividad}</p>
+        </div>
+
+        <div class="containButtons">
+            <img src="./resource/basura.png" alt="Icono de elimina actividad" class="buttons buttonX" title="Eliminar">
+            <img src="./resource/lapiz.png" alt="Icono de editar actividad" class="buttons buttonE" title="Editar">
+        </div>
+    </article>
+    `;
+}
+
+const btn = document.querySelector(".button");
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    let inputTitle = document.getElementById("inputTitle");
+    let inputDescription = document.getElementById("inputDescription");
+    let contenedor = document.getElementById("datos");
+
+    let tituloLimpio = inputTitle.value.trim();
+    let descripcionLimpia = inputDescription.value.trim();
+
+    if (tituloLimpio === "" || descripcionLimpia === "") {
+        alert("Por favor, llena todos los campos");
+        return;
+    }
+
+    contenedor.insertAdjacentHTML('afterbegin', crearPlantilla(inputTitle.value, inputDescription.value));
+
+    inputTitle.value = "";
+    inputDescription.value = "";
+});
